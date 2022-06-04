07638e31453f2b232713137df6311b973379835c
Azarill <heierms@gmail.com>
2022-06-04 00:01:05 -0500

    setup project

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS company_cms;
+CREATE DATABASE company_cms;
+
+USE company_cms;
+
+CREATE TABLE departments(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    department_id INTEGER NOT NULL,
+    salary INTEGER NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES departments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title_id INTEGER NOT NULL,
+    manager VARCHAR(30),
+    FOREIGN KEY(job_title_id) REFERENCES roles(id) ON DELETE CASCADE
+);
+
